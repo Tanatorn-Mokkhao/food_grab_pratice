752f553d9539b5dda61b6478d587ef9c02f3012b
Tanatorn Mokkhao <tanatorn.mokkhao@gmail.com>
2022-04-29 17:34:15 +0700

    fix infra

--- auth_service/src/app.ts
+++ auth_service/src/app.ts
@@ -5,6 +5,10 @@ import {ErrorHandle, NotfoundError } from '@martfoodgrab/common'
 const app = express()
 app.use(express.json())
 
+app.use('/api/users/test', (req, res) => { 
+    console.log('test')
+    res.send("his")
+})
 
 app.all('*', () => { 
     throw new NotfoundError()
