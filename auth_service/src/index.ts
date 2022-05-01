8836b214a7cb3306db788ed3c88e1a73f8b662b4
Tanatorn Mokkhao <tanatorn.mokkhao@gmail.com>
2022-05-01 10:54:50 +0700

    add error env

--- auth_service/src/index.ts
+++ auth_service/src/index.ts
@@ -12,6 +12,10 @@ const start = async () => {
         throw new Error('JWT_KEY must be defined')
     }
 
+    if (!process.env.TEST_ERROR) { 
+        throw new Error('TEST_ERROR must be defined')
+    }
+
     try {
         await mongoose.connect(process.env.MONGO_URL)
         console.log('connected to mongodb')
